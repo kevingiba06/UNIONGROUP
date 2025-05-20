06b56d54e3efdf0ca100b1e8332f2a3fb4f46a20
SVillaluz <sebastianvillaluz99@gmail.com>
2025-05-20 09:39:10 -0600

    Se agrego el logo nuevamente

--- uniongroup/src/App.js
+++ uniongroup/src/App.js
@@ -1,26 +1,22 @@
-import logo from './assets/logo_footer_2.svg';
+import logo from './logo_footer_2.svg';
 import './App.css';
-import fondo from './src/assets/fondo.jpg'; // Asegúrate de agregar tu imagen en `src/assets`
- 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
   );
 }
- 
+
 export default App;
