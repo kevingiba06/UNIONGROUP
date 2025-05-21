d92c1d416f0ab783aa4517fb0ff2a99c7613293d
SVillaluz <sebastianvillaluz99@gmail.com>
2025-05-21 13:53:40 -0600

    Se continuo con la edición de las paginas

--- uniongroup/src/Quienessomos.jsx
+++ uniongroup/src/Quienessomos.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Importa Link
 import './quienessomos.css';
+import backIcon from './assets/icons8-atras-48.png';
 
 function Quienessomos() {
   const currentYear = new Date().getFullYear();
@@ -12,26 +13,49 @@ function Quienessomos() {
           <Link to={"/que-hacemos"}>QUE HACEMOS</Link>
           <Link to={"/marcas-clientes"}>MARCAS & CLIENTES</Link>
           <Link to={"/contacto"}>CONTACTO</Link>
-          <Link to={"/"}>REGRESAR</Link>
+          <Link to={"/"}>
+            <img src={backIcon} alt="Regresar" className="icon-svg" />
+          </Link>
+        </div>
+      </div>
+
+      <div className='center'>
+        <div className='title'>
+          QUE HACEMOS
+        </div>
+        <div className='info'>
+          Gestión de Marcas Globales, Desarrollo y diseño de producto, Sourcing, Licencias, Marketing y Ventas a través de tiendas , e-commerce y wholesale
+        </div>
+        <div className='title'>
+          M
+        </div>
+        <div className='info'>
+          Gestión de Marcas Globales, Desarrollo y diseño de producto, Sourcing, Licencias, Marketing y Ventas a través de tiendas , e-commerce y wholesale
+        </div>
+        <div className='title'>
+          V
+        </div>
+        <div className='info'>
+          Gestión de Marcas Globales, Desarrollo y diseño de producto, Sourcing, Licencias, Marketing y Ventas a través de tiendas , e-commerce y wholesale
         </div>
       </div>
 
       <div className='info-email-logo'>
-                <div className='info'>
-                    <p>Alianzas globales, relaciones sólidas</p>
-                </div>
-                <div className='email'>
-                    recursoshumanos@uniongroup.mx
-                </div>
-                <div className='years-logo-container'>
-                    <span className='year'>1980</span>
-                    <div className='police'>
-                        <div className='logo2_1'></div>
-                        <p>© Privacy Police</p>
-                    </div>
-                    <span className='year'>{currentYear}</span>
-                </div>
-            </div>
+        <div className='info'>
+          <p>Alianzas globales, relaciones sólidas</p>
+        </div>
+        <div className='email'>
+          recursoshumanos@uniongroup.mx
+        </div>
+        <div className='years-logo-container'>
+          <span className='year'>1980</span>
+          <div className='police'>
+            <div className='logo2_1'></div>
+            <p>© Privacy Police</p>
+          </div>
+          <span className='year'>{currentYear}</span>
+        </div>
+      </div>
     </div>
   );
 }
